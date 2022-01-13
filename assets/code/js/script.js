000b78061a8df4784736ac0163eef448031871f6
xndrbrgs <aborgescolon@gmail.com>
2022-01-13 11:36:49 -0500

    added second js

--- assets/code/js/script.js
+++ assets/code/js/script.js
@@ -2,9 +2,3 @@
 
 var scene = document.getElementById('scene');
 var parallaxInstance = new Parallax(scene);
-
-$(".button").click(function() {
-    window.location = $(this).find("a").attr("href");
-    console.log("click");
-    return false;
-  });
