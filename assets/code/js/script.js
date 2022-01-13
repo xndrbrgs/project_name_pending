fb4ecbd09b400f4e982993b0708901cdc236fb1b
xndrbrgs <aborgescolon@gmail.com>
2022-01-12 19:01:19 -0500

    button fixed

--- assets/code/js/script.js
+++ assets/code/js/script.js
@@ -2,3 +2,9 @@
 
 var scene = document.getElementById('scene');
 var parallaxInstance = new Parallax(scene);
+
+$(".button").click(function() {
+    window.location = $(this).find("a").attr("href");
+    console.log("click");
+    return false;
+  });
